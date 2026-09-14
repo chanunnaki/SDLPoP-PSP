1f67b526f8ad2047a04dcb9ea9a37210de925a23
chanunnaki <nom.chan@me.com>
2026-09-14 15:12:22 +0700

    Restore authentic DOS behavior: small potion chime and strobe only trigger when injured

--- src/seg000.c
+++ src/seg000.c
@@ -839,11 +839,11 @@ int process_key() {
 				}
 			break;
 			case SDL_SCANCODE_S | WITH_SHIFT: // Shift+S
-				play_sound(sound_33_small_potion); // small potion (cheat)
-				flash_color = color_12_brightred;
-				flash_time = 2;
 				if (hitp_curr != hitp_max) {
+					play_sound(sound_33_small_potion); // small potion (cheat)
 					hitp_delta = 1;
+					flash_color = color_12_brightred;
+					flash_time = 2;
 				}
 			break;
 			case SDL_SCANCODE_T | WITH_SHIFT: // Shift+T
